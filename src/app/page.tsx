dc30d026d001eaf2233a7524a61df908015bd580
Sonja-Kitanoska <avramoskamkd@gmail.com>
2023-08-07 10:06:03 +0200

    small things for mobile responssiveness for home page fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Home = () => {
   return (
     <main className=" mt-20 mb-4 darkBlue min-h-screen ">
-      <div className="flex flex-col md:flex-row  gap-10 mt-36 px-6 sm:px-10 w-full ">
-        <div className="">
+      <div className="flex flex-col md:flex-row md:justify-items-center md:items-center gap-10 mt-36 px-6 sm:px-10 w-full md:mt-48">
+        <div className="w-1/2">
           <Image
             width={600}
             height={960}
@@ -16,7 +16,7 @@ const Home = () => {
           />
         </div>
 
-        <div className="flex flex-col items-start gap-1 w-1/2 w-full sm:w-full">
+        <div className="flex flex-col items-start gap-1 w-1/2 w-full sm:w-full md:mt-16">
           <div>
             <h1 className="font-black text-3xl orange">Hello,</h1>
             <h1 className="font-black text-3xl orange mt-2">
@@ -27,16 +27,16 @@ const Home = () => {
             A full-stack developer based in Stockholm. The best developer on
             planet Earth. I haven't been to other planets yet.
           </p>
-        </div>
-        <div className="w-1/2 sm:w-1/6 flex">
-          <Link href="mailto:avramoskamkd@gmail.com">
-            <button
-              type="button"
-              className="text-white justify-self-start bg-blue w-full rounded-xl px-4 py-1"
-            >
-              Email me
-            </button>
-          </Link>
+          <div className="w-1/2 sm:w-1/6 flex mt-2">
+            <Link href="mailto:avramoskamkd@gmail.com">
+              <button
+                type="button"
+                className="text-white justify-self-start bg-blue w-full rounded-xl px-4 py-1"
+              >
+                Email me
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </main>
